0b00e979333ca7ff58b564c5da3f6fb3646dfdff
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-05-18 20:36:08 -0400

    hooked up records endpoint

--- routes/records.js
+++ routes/records.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db_connect");
 
 router.get("/", (req, res, next) => {
   console.log("root records endpoint hit");
   res.status(200).send("the root records endpoint");
+});
+
+router.post("/", (req, res, next) => {
+  const { searchField, searchTerm } = req.body;
+  console.log(searchTerm);
+  if (searchField.toLowerCase() === "artist") {
+    pool.query(
+      "select * from records where artist like $1",
+      [`%${searchTerm}%`],
+      (err, result) => {
+        res.status(200).send(result.rows);
+      }
+    );
+  } else if (searchField.toLowerCase() === "title") {
+    pool.query(
+      "select * from records where title like $1",
+      [`%${searchTerm}%`],
+      (err, result) => {
+        res.status(200).send(result.rows);
+      }
+    );
+  } else if (searchField.toLowerCase() === "location") {
+    pool.query(
+      "select * from records where location like $1",
+      [`%${searchTerm}%`],
+      (err, result) => {
+        res.status(200).send(result.rows);
+      }
+    );
+  }
 });
 
 module.exports = router;
